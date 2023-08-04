fcc39e88825e459d4da22fef60519e140b39b040
Alex Schittko <alex4108@live.com>
2023-08-04 21:57:09 +0000

    Customize content

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LOGO</a>
+            <a href="."><img src="./assets/icons/stw.png" height="100" width="100" /></a>
           </div>
           <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
+            {/* <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
               <IconFacebook className="icon" />
             </a>
             <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
@@ -21,7 +21,7 @@ class App extends React.Component {
             </a>
             <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
               <IconGithub className="icon" />
-            </a>
+            </a> */}
           </div>
         </div>
         <div className="content">
@@ -29,13 +29,13 @@ class App extends React.Component {
             <h1>Get ready for the change.</h1>
             <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
           </div>
-          <a href="mailto:user@example.com">
-            <div className="cta">Send us an email</div>
+          <a href="https://forms.office.com/r/EQithvCrBt">
+            <div className="cta">Sign up for our Newsletter</div>
           </a>
         </div>
-        <div className="footer">
+        {/* <div className="footer">
           <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
-        </div>
+        </div> */}
       </div>
     );
   }
